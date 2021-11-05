ac5ccbd02c7e0ecc8dd3651fb249a3b5f8b103bf
kriskoin <cris7775@gmail.com>
2021-11-04 18:13:31 -0600

    create close welcome actions, plus ricardian

---
+++ ricardian/tictactoe123.contracts.md
@@ -0,0 +1,20 @@
+<h1 class="contract">welcome</h1>
+---
+spec-version: 0.1.0
+title: Welcome
+summary: The intent of the `{{ welcome }}` action is welcome a player
+icon: https://favicon.ico
+
+<h1 class="contract">create</h1>
+---
+spec-version: 0.1.0
+title: create
+summary: The intent of the `{{ create }}` action is create a game 
+icon: https://favicon.ico
+
+<h1 class="contract">close</h1>
+---
+spec-version: 0.1.0
+title: close
+summary: The intent of the `{{ close }}` action is close a game 
+icon: https://favicon.ico
---
+++ tictactoe123.cpp
@@ -0,0 +1,59 @@
+#include <eosio/eosio.hpp>
+
+using namespace eosio;
+CONTRACT tictactoe123 : public contract {
+    public:
+        using contract::contract;
+        TABLE game {
+            uint128_t id;
+            name challenger;
+            name host;
+            uint128_t primary_key() const { return id; }
+            uint64_t  by_challenger() const { return challenger.value; }
+            EOSLIB_SERIALIZE( game, (id)(challenger)(host))
+        };
+
+      typedef eosio::multi_index<name("games"), game,
+       eosio::indexed_by<name("idxchall"), eosio::const_mem_fun<game, uint64_t, &game::by_challenger>>
+      > games_table;
+
+        ACTION welcome(name host , name opponent){
+            require_auth(get_self());
+            print ("Hola amigos");
+        }
+
+        ACTION create(  name challenger, name host ){
+            require_auth(host);
+            check(challenger != host,"challenger and host must be diferent");
+            games_table games(get_self(),get_self().value);
+
+            //checks for the chagenller
+            auto idx_chg = games.get_index<name("idxchall")>();
+            auto tmp_chg = idx_chg.lower_bound(challenger.value);
+            check(tmp_chg ==idx_chg.end(),"Challender already exits"); 
+
+            uint128_t tmp_key = uint128_t{host.value} << 64 | challenger.value;
+            auto record = games.find( tmp_key);
+            if( record == games.end() ){
+                games.emplace(get_self(), [&]( auto& row ) {
+                    row.id = tmp_key;
+                    row.challenger = challenger;
+                    row.host = host;  
+                });
+            }else{
+                print ("game already exits");
+            }
+        }
+
+       ACTION close( const name challenger, const name host ){
+            require_auth(host);
+            check(challenger != host,"challenger and host must be diferent");
+            games_table games(get_self(),get_self().value);
+            uint128_t tmp_key = uint128_t{host.value} << 64 | challenger.value;
+            auto record = games.find( tmp_key);
+            if( record != games.end() ){
+                record = games.erase(record);
+            }
+        }
+
+};
